fdcc5af8da5891155968a6a77920e472195fef9e
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-17 14:37:23 +0300

    Connected to the local host server successfully

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello there. Welcome to my server.")
+})
+
 app.listen("5000", () => {
   console.log("Server is ready and running.");
 })
