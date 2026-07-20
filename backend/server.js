17dabb635219a7a17dceadb920693b7c8c1a3798
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-20 13:14:32 +0300

    Created the DELETE request

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,18 @@ app.post("/api/notes", async (req, res) => {
     res.status(500).json({success: false, message:"Server error"});
   }
 
+})
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Note.findByIdAndDelete(id);
+    res.status(200).json({success: true, message: "Note successfully deleted."})
+  } catch (error) {
+    console.error("Error creating note:", error.message);
+    res.status(500).json({success: false, message: "Server error."})
+  }
 
 })
 
