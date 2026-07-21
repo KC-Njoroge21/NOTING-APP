a1f3bbe79218f25670e2ee7e2734b1a7a8b84dd3
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-21 11:24:33 +0300

    Installed react icons

---
+++ frontend/src/components/HomeHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeHeader = () => {
+  return (
+    <header>
+      <div>
+        <h4>NOTES APP</h4>
+        <H3>Your notes</H3>
+      </div>
+       <button>
+        New note
+       </button>
+    </header>
+  )
+}
+
+export default HomeHeader
